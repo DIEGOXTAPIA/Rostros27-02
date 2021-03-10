221f629079fb409ac2354131b33aeee074a50732
Diego Xavier Tapia Inchauste <a2018118218@estudiantes.upsa.edu.bo>
2021-03-10 11:47:31 -0400

    FInalizacion programa rostros 27.02

---
+++ solrostros 27.02/rostros27.02.21/Models/Identity.cs
@@ -0,0 +1,11 @@
+﻿namespace rostros27._02._21.Models
+{
+    public class Identity
+    {
+        public int IdentityID { get; set; }
+        public string Name { get; set; }
+        public string Photo { get; set; }
+        public string Sex { get; set; }
+        
+    }
+}
